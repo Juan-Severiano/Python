decc87c11b6e04aca6709a7b709c7ad96ded8707
Juan <juansoussev@gmail.com>
2022-10-19 19:47:44 -0300

    Ta no rumo, PS: terminar depois pq ta  ficando legal

--- macdonalds.py
+++ macdonalds.py
@@ -18,6 +18,9 @@ Nossas opçoes de Refrigerante:
 burgercarne = 0
 burgerqueijo = 0
 burgerveg = 0
+coca = 0
+fanta = 0
+guarana = 0
 
 
 
@@ -25,32 +28,76 @@ i = input('Voce quer fazer um pedido? S/N: \n').upper()
 
 if (i == 'S') or ( i == 'SIM') or (i == 'YES'):
     while True:
-        print(cardapio)
-        
-        f = input('Qual será seu pedido? ')
-        if (f == '1')or(f == '2')or(f == '3'):
-            if f == '1':
-                print('Voce escolheu um Burger de Carne')
-                burgercarne += 1
-                J = input('Voce quer pedir mais alguma coisa? S/N\n').upper()
-                if J == 'N':
-                    break
-            elif f == '2':
-                print('Voce escolheu um Burger de Queijo')
-                burgerqueijo += 1
-                J = input('Voce quer pedir mais alguma coisa? S/N\n').upper()
-                if J == 'N':
-                    break
-            elif f == '3':
-                print('Voce escolheu um BUrger Vegetariano')
-                burgerveg += 1
-                J = input('Voce quer pedir mais alguma coisa? S/N\n').upper()
-                if J == 'N':
-                    break
-        else:
-            print('Digite um valor valido')
-    print(f'Voce pediu {burgercarne} Hamburger(s) de Carne\nVoce pediu {burgerqueijo} Hamburger(s) de Queijo\nVoce pediu {burgerveg} Hamburger(s) Vegetarianos')
-    print('Obrigado pela preferencia, volte sempre')
+        s = input("""
+            Voce vai querer 
+            [1] Hamburger
+            [2] Refrigerante
+            [3] Nenhum dos dois
+        """)
+        if s == "1":
+            while True:
+                print(cardapio)
+                f = input('Qual será seu pedido? ')
+                if (f == '1')or(f == '2')or(f == '3'):
+                    if f == '1':
+                        print('Voce escolheu um Burger de Carne')
+                        burgercarne += 1
+                        J = input('Voce quer mais hamburgers? S/N\n').upper()
+                        if J == 'N':
+                            break
+
+                    elif f == '2':
+                        print('Voce escolheu um Burger de Queijo')
+                        burgerqueijo += 1
+                        J = input('Voce quer mais hamburgers? S/N\n').upper()
+                        if J == 'N':
+                            break
+                    elif f == '3':
+                        print('Voce escolheu um BUrger Vegetariano')
+                        burgerveg += 1
+                        J = input('Voce quer mais hamburgers? S/N\n').upper()
+                        if J == 'N':
+                            break
+
+        elif s == "2" :
+            while True:
+                print (cardapio_refri)
+                f = input('Qual será seu pedido? ')
+                if (f == '1')or(f == '2')or(f == '3'):
+                    if f == '1':
+                        print('Voce escolheu uma Coca Cola')
+                        coca += 1
+                        J = input('Voce quer mais algum refrigerante? S/N\n').upper()
+                        if J == 'N':
+                            break
+
+
+                    elif f == '2':
+                        print('Voce escolheu uma Fanta')
+                        fanta += 1
+                        J = input('Voce quer mais algum refrigerante? S/N\n').upper()
+                        if J == 'N':
+                            break
+                    elif f == '3':
+                        print('Voce escolheu o Guaraná')
+                        guarana += 1
+                        J = input('Voce quer mais algum refrigerante? S/N\n').upper()
+                        if J == 'N':
+                            break
+                else:
+                    print('Digite um valor valido')
+
+        elif s =='3':
+            break  
+    print(f"""
+        Voce pediu {burgercarne} Hamburger(s) de Carne
+        Voce pediu {burgerqueijo} Hamburger(s) de Queijo
+        Voce pediu {burgerveg} Hamburger(s) Vegetarianos
+        Voce pediu {coca} Refrigerante(s) de Coca
+        Voce pediu {fanta} Refrigerantess de Fanta
+        Voce pediu {guarana} Refrigerantes de Guaraná
+    
+    """)
 elif (i == 'N') or (i == 'NAO') or (i == 'NÃO') or (i == 'NAOP') or (i == 'QUERO NAO BB') :
     print('Ate mais')
 
